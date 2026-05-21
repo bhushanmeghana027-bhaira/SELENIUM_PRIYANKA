79ebf651d6a0d2d8686fcfb3b0330a484bed4029
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-05-21 08:26:26 +0530

    dimenision changing

---
+++ src/test/java/day1/P2_ALTERINGDIMENSIONS.java
@@ -0,0 +1,23 @@
+package day1;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P2_ALTERINGDIMENSIONS {
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver = new ChromeDriver();
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.engineerdiaries.com/selenium");
+		Thread.sleep(2000);
+		driver.manage().window().minimize();
+		Thread.sleep(2000);
+		driver.manage().window().fullscreen();
+		Thread.sleep(2000);
+		driver.manage().window().setSize(new Dimension(500, 900));
+		Thread.sleep(2000);
+		driver.manage().window().setPosition(new Point(50, 200));
+	}
+}
